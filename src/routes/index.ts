87615a5c0c73d54ea515a4b3f2f1b5c103416870
w2story(이건희) <w2story@naver.com>
2025-01-19 00:24:05 +0900

    2025-01-19 item-auto-setup-controller

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import test from './test/router';
 import game from './game/router';
+import item from './item/router';
 import character from './character/router';
 
 const routes = Router();
@@ -8,6 +9,7 @@ const routes = Router();
 // 라우터 분기시 사용
 routes.use('/api/v0/test/', test);
 routes.use('/api/v0/game/', game);
+routes.use('/api/v0/item/', item);
 routes.use('/api/v0/character/', character);
 
 export default routes;
--- src/routes/item/router.ts
+++ src/routes/item/router.ts
@@ -1,16 +1,16 @@
 import express from 'express';
 
 // 컨트롤러 참조
-import TestController from './itemTestController';
+import ItemTestController from './itemTestController';
 
 const router = express.Router();
 
 //router.use(cors(corsOpt));
 
 // 요청 method 별로 라우팅itemSet
-router.get('/test/RelicsSet', TestController.itemRelicsSet);
-router.get('/test/PlanetarySet', TestController.itemPlanetarySet);
-router.get('/test/Set', TestController.itemSet);
-router.get('/test/search', TestController.itemSearch);
+router.get('/test/RelicsSet', ItemTestController.itemRelicsSet);
+router.get('/test/itemSet', ItemTestController.itemSet);
+router.get('/test/search', ItemTestController.itemSearch);
+router.get('/test/cardSet', ItemTestController.itemCardSet);
 
 export default router;
